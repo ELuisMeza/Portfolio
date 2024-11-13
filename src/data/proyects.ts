34dea956234c7e24cb129a286178ee07ad7e88ea
Luis Meza <ednilsonluis.11@gmail.com>
2024-11-13 00:44:38 -0500

    Add Links Proyects

--- src/data/proyects.ts
+++ src/data/proyects.ts
@@ -13,8 +13,8 @@ export const all_proyects: Type_Proyect[] = [
     description:
       "Maquetado de una página web para una agencia de marketing digital dedicada a potenciar el crecimiento y éxito de empresas en el entorno digital. En este proyecto lo realize con Astro, al ser un sitio web sin mucha interactividad, ademas practique el uso de la libreria Framer-Motion, la cual me ayudo a realizar las animaciones y componentes de una manera mas facil y legible.",
     technologies: ["Astro", "Tailwind CSS", "Typescript", "Framer-Motion"],
-    demoURL: "https://www.dutch.com",
-    repoURL: "dfs",
+    demoURL: "https://positivus-web1.netlify.app/",
+    repoURL: "https://github.com/luismeza1101/Positivus.git",
     image: 'Positivus'
   },
   {
@@ -22,8 +22,8 @@ export const all_proyects: Type_Proyect[] = [
     description:
       "Red social basica donde puedes conectar con mas personas y conversar sobre diversos temas, podrás crear posts y comentar los posts de los demás. Este proyecto lo realize con Next.js, al tener mucha interactivad. Ademas para la base de datos use SQLite al ser un proyecto pequeño, y para el Backend use FastAPI, con una estructura API RESTfull",
     technologies: ["Next.js", "Tailwind CSS", "Typescript", "Python", "FastAPI"],
-    demoURL: "https://www.absoluteweb.com",
-    repoURL: "dfs",
+    demoURL: "https://pulse-front-5ccjdok4w-luismeza1101s-projects.vercel.app",
+    repoURL: "https://github.com/luismeza1101/Pulse.git",
     image: 'Pulse'
   },
   {
@@ -31,8 +31,8 @@ export const all_proyects: Type_Proyect[] = [
     description:
       "Maquetado de un e-commerce dedicado a la venta de prendas de ropa, Creado con Next.js, por la interactividad que tiene con el usuario, junto con Typescrip para no tener tantos problemas con los tipos de los datos, Ademas con el uso del localStorage para tener el carrito de compras permanente",
     technologies: ["Next.js", "Tailwind CSS", "Typescript", "Framer-Motion"],
-    demoURL: "https://www.miinto.com",
-    repoURL: "dfs",
+    demoURL: "https://shopcoo1.netlify.app/",
+    repoURL: "https://github.com/luismeza1101/SHOP.CO.git",
     image: 'SHOP-CO'
   },
   {
@@ -40,8 +40,8 @@ export const all_proyects: Type_Proyect[] = [
     description:
       "Este sitio web esta desarrollado para que puedas saber la temperatura actual del lugar en el que te encuentras, ademas cuenta con 4 espacios vacios adicionales donde puedes tener el clima de la ciudad que quieras",
     technologies: ["Next.js", "Tailwind CSS", "Typescript"],
-    demoURL: "https://www.efigence.com",
-    repoURL: "dfs",
+    demoURL: "https://clima-sigma-dusky.vercel.app/",
+    repoURL: "https://github.com/luismeza1101/Clima.git",
     image: 'clima'
   },
   {
@@ -49,8 +49,8 @@ export const all_proyects: Type_Proyect[] = [
     description:
       "Desafio dificil recopilado del sitio web Frontend Mentor, que simula la seccion de un e-commerce de una tienda de zapatillas en linea",
     technologies: ["Next.js", "Tailwind CSS", "Typescript"],
-    demoURL: "https://www.efigence.com",
-    repoURL: "dfs",
+    demoURL: "https://sneaker-store-bice.vercel.app/",
+    repoURL: "https://github.com/luismeza1101/SneakerStore.git",
     image: 'sneakerShop'
   },
 ];
